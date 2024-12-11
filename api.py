ef0ee8c8d3e53ae9727167f173a2ee19db06d618
JovTim <202280218@psu.palawan.edu.ph>
2024-12-11 18:47:23 +0800

    initial api

--- api.py
+++ api.py
@@ -1,3 +1,11 @@
 from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
+app = Flask(__name__)
+
+@app.route("/")
+def greetings():
+    return "<p>Books and Libraries API</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
