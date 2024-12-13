55ae1f25e0a0d3d228662a33d047c11685528c5c
JovTim <202280218@psu.palawan.edu.ph>
2024-12-13 21:15:29 +0800

    token

--- api.py
+++ api.py
@@ -2,6 +2,16 @@ from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 from db_connection2 import db_information
 
+import jwt
+import datetime
+from functools import wraps
+
+from api_key import api_key
+
+
+
+#from data_generator.data import random_isbn
+
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = db_information(0)
 app.config["MYSQL_USER"] = db_information(1)
@@ -10,11 +20,30 @@ app.config["MYSQL_DB"] = db_information(3)
 
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+app.config['SECRET_KEY'] = api_key()
+
 mysql = MySQL(app)
 
 @app.route("/")
 def greetings():
     return "<p>Books and Libraries API</p>"
+
+# ------------ TOKEN -----
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired!'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token!'}), 401
+        return f(*args, **kwargs)
+    return decorated
+# -----------------------
 
 def data_fetch(query):
     cur = mysql.connection.cursor()
@@ -97,10 +126,34 @@ def get_book_by_id(id: int):
     data = data_fetch(query)
 
     return make_response(jsonify({"books.book_id": id, "count": len(data), "information": data}), 201)
-
+# isbn, book_title, date_of_publication, category_id, author_name
 @app.route("/book", methods=["POST"])
 def add_book():
-    ...
+    try:
+        cur = mysql.connection.cursor()
+        info = request.get_json()
+        isbn = info["isbn"]
+        book_title = info["book_title"]
+        date_of_publication = info["date_of_publication"]
+        category_id = info["category_id"]
+        author_name = info["author_name"]
+
+        query = """
+                INSERT INTO books_libraries.books(isbn, book_title, date_of_publication, category_id, author_name)
+                VALUES(%s, %s, %s, %s, %s);
+                """
+        
+        values = (isbn, book_title, date_of_publication, category_id, author_name)
+        cur.execute(query, values)
+        mysql.connection.commit()
+    except:
+        print("Error")
+        mysql.connection.rollback()
+    finally:
+        print("row(s) affected: {}".format(cur.rowcount))
+        rows_affected = cur.rowcount
+        cur.close()
+        return make_response(jsonify({"message": "book added successfully", "row_affected": rows_affected}))
 
 if __name__ == "__main__":
     app.run(debug=True)
